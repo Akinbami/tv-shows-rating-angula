9dbd9d9125e1d20c939e92359b8251c35957dc9b
Dayo Akinbami <akinbamizacheus#gmail.com>
2020-09-02 10:35:23 +0100

    added rating form

--- projects/show-rating/src/app/app.module.ts
+++ projects/show-rating/src/app/app.module.ts
@@ -4,13 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import "~bootstrap/scss/bootstrap";
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TvRatingFormComponent } from './tv-rating-form/tv-rating-form.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TvRatingFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
